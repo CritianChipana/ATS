d24dbac4e8cf52593086eea27e3b2ad45c68be3a
CritianChipana <cristianchipanahuaman@gmail.com>
2021-05-30 02:12:17 -0500

    hola- preguntas terminadas

---
+++ app/src/main/java/com/example/ats/PreguntaSimulacro.kt
@@ -0,0 +1,11 @@
+package com.example.ats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreguntaSimulacro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pregunta_simulacro)
+    }
+}
---
+++ app/src/main/java/com/example/ats/ResultadosSimulacion.kt
@@ -0,0 +1,11 @@
+package com.example.ats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadosSimulacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultados_simulacion)
+    }
+}
