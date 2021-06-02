7dc16401e12c8abbfbad738afc530cc283fee5a9
MariaAP10 <rosalynpfuturi@gmail.com>
2021-06-02 03:48:42 -0500

    Maria Parte 3

---
+++ app/src/main/java/com/example/ats/confirmar_practica.kt
@@ -0,0 +1,11 @@
+package com.example.ats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class confirmar_practica : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmarpractica)
+    }
+}
---
+++ app/src/main/java/com/example/ats/crear_cuenta.kt
@@ -0,0 +1,11 @@
+package com.example.ats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class crear_cuenta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_crearcuenta)
+    }
+}
