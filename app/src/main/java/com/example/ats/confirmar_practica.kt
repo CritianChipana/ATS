fa29b185a912f8a6e190268f3e9c1e8e97df519b
MariaAP10 <rosalynpfuturi@gmail.com>
2021-06-02 05:12:10 -0500

    Maria parte 5

--- app/src/main/java/com/example/ats/confirmar_practica.kt
+++ app/src/main/java/com/example/ats/confirmar_practica.kt
@@ -1,14 +1,13 @@
 package com.example.ats
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+
 
 class confirmar_practica : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        //val practica1 = Confirmar
-        //setContentView(R.layout.activity_confirmarpractica)
     }
 }
