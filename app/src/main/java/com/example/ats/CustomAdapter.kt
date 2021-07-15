1d2a9ff922272af14aa1036093e9f2eab1ebf870
CritianChipana <cristianchipanahuaman@gmail.com>
2021-07-15 10:26:13 -0500

    recyclerView con click terminado

--- app/src/main/java/com/example/ats/CustomAdapter.kt
+++ app/src/main/java/com/example/ats/CustomAdapter.kt
@@ -1,13 +1,18 @@
 package com.example.ats
 
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomAdapter : RecyclerView.Adapter< CustomAdapter.ViewHolder>(){
+class CustomAdapter( /*private val itemClickListener: AdapterView.OnItemClickListener*/
+                    ) : RecyclerView.Adapter< CustomAdapter.ViewHolder>(){
 
     val cursos = arrayOf(
                         "GEOMETRIA",
@@ -33,6 +38,10 @@ class CustomAdapter : RecyclerView.Adapter< CustomAdapter.ViewHolder>(){
             R.drawable.ico_paypal,
             R.drawable.icono_practi )
 
+    interface OnPracticaClickListener{
+        fun onItemClick()
+    }
+
     inner class ViewHolder( itemView:View ) :RecyclerView.ViewHolder( itemView ){
 
         var itemCurso : TextView =itemView.findViewById( R.id.txtCurso )
@@ -41,12 +50,20 @@ class CustomAdapter : RecyclerView.Adapter< CustomAdapter.ViewHolder>(){
             itemCurso.setOnClickListener(){
                 v: View ->
                 val position: Int = adapterPosition
-                val nombre = absoluteAdapterPosition
-                Toast.makeText(itemView.context, "Precionaste # ${position +1} mientras que ${nombre +1}",Toast.LENGTH_SHORT).show()
+                val positionActualizado = absoluteAdapterPosition
+                var bundle = Bundle().apply {
+                    val post = positionActualizado.toString()
+                    putString("key_curso",post)
+                }
+
+                val intent = Intent( itemView.context, confirmar_practica::class.java ).apply {
+                    putExtras( bundle )
+                }
+                itemView.context.startActivity( intent )
+                //Toast.makeText(itemView.context, "Precionaste # ${position +1} mientras que ${positionActualizado +1}",Toast.LENGTH_SHORT).show()
             }
         }
     }
-    
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): CustomAdapter.ViewHolder {
         val v = LayoutInflater.from( viewGroup.context ).inflate( R.layout.card_layout, viewGroup, false )
--- app/src/main/java/com/example/ats/confirmar_practica.kt
+++ app/src/main/java/com/example/ats/confirmar_practica.kt
@@ -3,11 +3,33 @@ package com.example.ats
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.example.ats.databinding.ActivityConfirmarpracticaBinding
 
 
 class confirmar_practica : AppCompatActivity() {
+
+    //private lateinit var binding : ActivityConfirmarpracticaBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+      val  binding = ActivityConfirmarpracticaBinding.inflate( layoutInflater )
         super.onCreate(savedInstanceState)
+        setContentView( binding.root )
+
+        val bundbleReception = intent.extras
+
+        if( bundbleReception != null ){
+            var id = bundbleReception!!.getString("key_curso")
+            println("*************************************************************")
+            println(id)
+
+            println("*************************************************************")
+            println("*************************************************************")
+            binding.txtId.setText( id )
+        }
+
+
     }
+
+
 }
